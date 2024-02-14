a0bb792d009254a149df183f3e7c421b2a55d275
liumingsheng <liumingsheng3411.outlook.com>
2024-02-14 23:00:39 +0800

    这是第三次了，已经写好了token，现在主要是把后端的所有api都整合起来方便前端使用

--- src/main/java/yang/opencampus/opencampusback/Rest.java
+++ src/main/java/yang/opencampus/opencampusback/Rest.java
@@ -3,11 +3,14 @@ package yang.opencampus.opencampusback;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import yang.opencampus.opencampusback.entity.Baseinfo;
 import yang.opencampus.opencampusback.entity.Comment;
 import yang.opencampus.opencampusback.entity.User;
 import yang.opencampus.opencampusback.service.MongoDB;
 import yang.opencampus.opencampusback.service.Mysql;
+import yang.opencampus.opencampusback.service.Token;
 
 import java.util.List;
 
@@ -31,18 +34,28 @@ public class Rest {
     public Rest(MongoDB mongo){
         this.mongo=mongo;
     }
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
-     @PostMapping("/register") 
-     public String register(@RequestParam String email,@RequestParam String password)
-{     
 
-    User yang=new User(email,password);
-    mysqldb.register(yang);
-    return "registered"; 
-} 
+     @PostMapping("/register") 
+     public String register(@RequestParam String email,@RequestParam String password,HttpServletResponse response)
+    {     
+
+    User newUser=new User(email,password);
+    boolean succesed;
+    succesed=mysqldb.register(newUser);
+    if(succesed){
+        Cookie cookie=new Cookie("JWT",Token.generateJWT(email));
+        response.addCookie(cookie);
+        return "registered"; 
+    }else{
+        return "failed";
+    }
+
+    } 
     @PostMapping("/login")
     public boolean login(@RequestParam String email,@RequestParam String password) {
         return mysqldb.login(email, password);
@@ -59,6 +72,11 @@ public class Rest {
     @PostMapping("/isagree")
     public void postMethodName(@RequestParam String commentID,@RequestParam boolean isagree) {
         mongo.updateComment(isagree,commentID);
+    }
+    @PostMapping("/check")
+    public boolean check(@RequestParam String JWT) {
+        return Token.checkToken( JWT);
+
     }
     
     
---
+++ src/main/java/yang/opencampus/opencampusback/service/Token.java
@@ -0,0 +1,134 @@
+package yang.opencampus.opencampusback.service;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Token {
+    @Value("${expiredDays}")
+    static public int expiredDays;
+    @Value("${securityCode}")
+    static public String securityCode;
+    static public String generateJWT(String email){
+        
+        LocalDateTime expiredTime=LocalDateTime.now();
+        expiredTime.plusDays(expiredDays);
+        Map<String, Object> payloadData = new HashMap<>();
+        payloadData.put("email", email);
+        payloadData.put("expiredTime", expiredTime.toString());
+        payloadData.put("pleaseDontUseSpider", "ItWillTakeALotOf$");
+        // 使用Jackson库将Map对象转换为JSON字符串
+        ObjectMapper mapper = new ObjectMapper();
+        String payloadJson=new String();
+        try {
+            payloadJson = mapper.writeValueAsString(payloadData);
+            System.out.println("payloadjson:"+payloadJson);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        try {
+            // 创建SHA-256的MessageDigest对象
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            // 将数据转换为字节数组并计算哈希值
+            byte[] hashBytes = digest.digest((payloadJson+securityCode).getBytes());
+            // 将哈希值转换为十六进制字符串
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                // 将每个字节转换为两位十六进制
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            // 打印SHA-256哈希值
+            System.out.println("payloadJSON" + payloadJson);
+            String signature = Base64.getEncoder().encodeToString(hexString.toString().getBytes());
+            String payload=Base64.getEncoder().encodeToString(payloadJson.getBytes());
+            return payload+"."+signature;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
+
+
+    static public boolean checkToken(String token){
+        
+        String[] parts = token.split("\\.");
+        String payloadJson = parts[0];
+        String sinature = parts[1];
+
+        
+        // 对Base64编码后的payload进行解码
+        byte[] decodedBytes = Base64.getDecoder().decode(payloadJson);
+        String decodedPayloadJson = new String(decodedBytes);
+        System.out.println("Base64解码后的Payload JSON字符串: " + decodedPayloadJson);
+        ObjectMapper decodemapper = new ObjectMapper();
+        // 将解码后的JSON字符串转换回Map对象
+        try {
+            Map<String, Object> decodedPayload = decodemapper.readValue(decodedPayloadJson, Map.class);
+            System.out.println("解码后的Payload Map: " + decodedPayload);
+            System.out.println(decodedPayload.get("email"));
+
+            String payloadEmail=(String) decodedPayload.get("email");
+            LocalDateTime payloadExpiredTime=LocalDateTime.parse((String) decodedPayload.get("expiredTime"));
+
+            System.out.println(payloadEmail+" get and get  " + payloadExpiredTime.toString());
+
+            
+        Map<String, Object> payloadData = new HashMap<>();
+        payloadData.put("email", payloadEmail);
+        payloadData.put("expiredTime", payloadExpiredTime.toString());
+        payloadData.put("pleaseDontUseSpider", "ItWillTakeALotOf$");
+        // 使用Jackson库将Map对象转换为JSON字符串
+        ObjectMapper encodemapper = new ObjectMapper();
+        String repayloadJson=new String();
+        try {
+            repayloadJson = encodemapper.writeValueAsString(payloadData);
+            System.out.println("in try re"+repayloadJson);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest((repayloadJson+securityCode).getBytes());
+            System.out.println("out try re"+repayloadJson);
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            // 打印SHA-256哈希值
+            System.out.println("remake SHA-256 哈希值: " + hexString.toString()+"sign:"+sinature);
+            String remakeSignatrue = Base64.getEncoder().encodeToString(hexString.toString().getBytes());
+
+
+
+            if(payloadExpiredTime.isAfter(LocalDateTime.now())){
+                return false;//超时
+            }else{
+                return remakeSignatrue.equals(sinature);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        
+        return true;
+    }
+}
